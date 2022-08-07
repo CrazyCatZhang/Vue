7b93a3e830e96f8900387ee0336360f0be1fa3ff
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-07 10:11:06 +0800

    single file component

---
+++ Componentization/demo/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+		    <HelloT></HelloT>
+		    <hr>
+        <School></School>
+    </div>
+</template>
+
+<script>
+import School from "./component/School.Vue";
+import HelloT from "./component/HelloT";
+
+export default {
+		components: {
+				School,
+				HelloT
+		},
+}
+</script>
+
+<style scoped>
+
+</style>
