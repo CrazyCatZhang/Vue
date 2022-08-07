d19cf50e3f90f41220284439d619153b059bb378
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-07 13:56:13 +0800

    props configuration item

---
+++ Vue-CLI/base-property/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+		<div id="app">
+				<img alt="Vue logo" src="./assets/logo.png">
+				<Student name="Cat" sex="Female" :age="18"/>
+				<Student name="CatZhang" sex="Male" :age="22"/>
+		</div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+
+export default {
+		name: 'App',
+		components: {
+				Student
+		}
+}
+</script>
+
+<style>
+#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+}
+</style>
---
+++ Vue-CLI/base-property/src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+		<div>
+				<h1>{{ msg }}</h1>
+				<h2>学生姓名：{{ name }}</h2>
+				<h2>学生性别：{{ sex }}</h2>
+				<h2>学生年龄：{{ myAge }}</h2>
+		</div>
+</template>
+
+<script>
+export default {
+		data() {
+				return {
+						msg: '我是一个USETC的学生',
+						myAge: this.age
+				}
+		},
+		props: ['name', 'sex', 'age']
+}
+</script>
+
+<style scoped>
+
+</style>
