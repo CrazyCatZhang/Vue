f59a65d267000784ecc1cf43a6c1addfe8c31942
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-07 21:45:05 +0800

    Communication from child components to parent components

---
+++ Vue-CLI/component-communication/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+		<div id="app">
+				<img alt="Vue logo" src="./assets/logo.png">
+				<Student :getStudentName="getStudentName" @demo="getStudentName2" ref="student" @click.native="output"/>
+		</div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+
+export default {
+		name: 'App',
+		components: {
+				Student
+		},
+		methods: {
+				getStudentName(val) {
+						console.log('学生姓名为：', val)
+				},
+				getStudentName2(val) {
+						console.log('学生姓名为：', val)
+				},
+				output() {
+						alert('111111111')
+				}
+		},
+		mounted() {
+				this.$refs.student.$on('test', (val) => {
+						console.log('学生姓名为：', val)
+				})
+		}
+}
+</script>
+
+<style>
+#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+}
+</style>
---
+++ Vue-CLI/component-communication/src/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+		<div>
+				<h2>{{ name }}</h2>
+				<h2>{{ age }}</h2>
+				<button @click="sendStudentName">点我传递姓名</button>
+				<button @click="sendStudentName2">点我传递姓名2</button>
+				<button @click="sendStudentName3">点我传递姓名3</button>
+		</div>
+</template>
+
+<script>
+export default {
+		data() {
+				return {
+						name: 'CatZhang',
+						age: 18
+				}
+		},
+		methods: {
+				sendStudentName() {
+						this.getStudentName(this.name)
+				},
+				sendStudentName2() {
+						this.$emit('demo', this.name)
+				},
+				sendStudentName3() {
+						this.$emit('test', this.name)
+				}
+		},
+		props: ['getStudentName']
+}
+</script>
+
+<style scoped>
+
+</style>
