e6346cc8a081596ff18b2262002cfbafb9753184
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-13 19:13:35 +0800

    Getters

--- Vue-CLI/vuex-demo/src/store/index.js
+++ Vue-CLI/vuex-demo/src/store/index.js
@@ -27,8 +27,15 @@ const state = {
     sum: 0
 }
 
+const getters = {
+    bigSum(state) {
+        return state.sum * 10
+    }
+}
+
 export default new Vuex.Store({
     actions,
     mutations,
-    state
+    state,
+    getters
 })
