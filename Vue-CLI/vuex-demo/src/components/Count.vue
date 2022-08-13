b1149785ece1222a1428388268fd2a22666ec60b
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-13 23:08:32 +0800

    Map Function

--- Vue-CLI/vuex-demo/src/components/Count.vue
+++ Vue-CLI/vuex-demo/src/components/Count.vue
@@ -1,20 +1,22 @@
 <template>
     <div>
-        <h1>当前求和为：{{ $store.state.sum }}</h1>
-        <h1>当前求和为的10倍为：{{ $store.getters.bigSum }}</h1>
+        <h1>当前求和为：{{ sum }}</h1>
+        <h1>当前求和为的10倍为：{{ bigSum }}</h1>
         <select v-model.number="n">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
         </select>
-        <button @click="increment">+</button>
-        <button @click="decrement">-</button>
-        <button @click="incrementOdd">当前求和为奇数再加</button>
-        <button @click="incrementWait">等一等再加</button>
+        <button @click="INCREMENT(n)">+</button>
+        <button @click="DECREMENT(n)">-</button>
+        <button @click="incrementOdd(n)">当前求和为奇数再加</button>
+        <button @click="incrementWait(n)">等一等再加</button>
     </div>
 </template>
 
 <script>
+import {mapState, mapGetters, mapActions, mapMutations} from "vuex";
+
 export default {
     name: 'Count',
     data() {
@@ -22,19 +24,13 @@ export default {
             n: 1, //用户选择的数字
         }
     },
+    computed: {
+        ...mapState(['sum']),
+        ...mapGetters(['bigSum'])
+    },
     methods: {
-        increment() {
-            this.$store.commit('INCREMENT', this.n)
-        },
-        decrement() {
-            this.$store.commit('DECREMENT', this.n)
-        },
-        incrementOdd() {
-            this.$store.dispatch('incrementOdd', this.n)
-        },
-        incrementWait() {
-            this.$store.dispatch('incrementWait', this.n)
-        },
+        ...mapActions({incrementOdd: 'incrementOdd', incrementWait: 'incrementWait'}),
+        ...mapMutations({INCREMENT: 'INCREMENT', DECREMENT: 'DECREMENT'})
     }
 }
 </script>
