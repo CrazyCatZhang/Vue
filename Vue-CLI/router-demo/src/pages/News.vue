2deaeeef543e6abe33508db604020a4fb126f806
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-14 16:37:02 +0800

    Activated and deactivated

--- Vue-CLI/router-demo/src/pages/News.vue
+++ Vue-CLI/router-demo/src/pages/News.vue
@@ -1,5 +1,6 @@
 <template>
     <ul>
+        <li :style="{opacity}">CatZhang</li>
         <li>news001 <input type="text"></li>
         <li>news002 <input type="text"></li>
         <li>news003 <input type="text"></li>
@@ -8,6 +9,20 @@
 
 <script>
 export default {
-    name: 'News'
+    name: 'News',
+    data() {
+        return {
+            opacity: 1
+        }
+    },
+    activated() {
+        this.timer = setInterval(() => {
+            this.opacity -= 0.01
+            if (this.opacity <= 0) this.opacity = 1
+        }, 16)
+    },
+    deactivated() {
+        clearInterval(this.timer)
+    }
 }
 </script>
