c255d19c2e4042890b6865a7a1db8f2f351f145a
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-08-14 19:39:19 +0800

    Global Route Guard

--- Vue-CLI/router-demo/src/router/index.js
+++ Vue-CLI/router-demo/src/router/index.js
@@ -6,34 +6,40 @@ import News from "@/pages/News";
 import Message from "@/pages/Message";
 import Detail from "@/pages/Detail";
 
-export default new VueRouter({
+const router = new VueRouter({
     routes: [
         {
             path: '/about',
-            component: About
+            component: About,
+            meta: {title: '关于'}
         },
         {
             path: '/home',
             component: Home,
+            meta: {title: '主页'},
             children: [
                 {
                     path: 'news',
-                    component: News
+                    component: News,
+                    meta: {title: '新闻', isAuth: true}
                 },
                 {
                     path: 'message',
                     component: Message,
+                    meta: {title: '消息', isAuth: true},
                     children: [
                         {
                             name: 'detail',
                             path: 'detail/:id/:title',
                             component: Detail,
+                            meta: {title: '详情', isAuth: true},
                             props({params}) {
                                 return {
                                     id: params.id,
                                     title: params.title
                                 }
                             }
+
                         }
                     ]
                 }
@@ -41,3 +47,18 @@ export default new VueRouter({
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.isAuth) {
+        if (localStorage.getItem('name') === 'CatZhang') next()
+        else alert('用户名不对...')
+    } else {
+        next()
+    }
+})
+
+router.afterEach((to) => {
+    document.title = to.meta.title
+})
+
+export default router
