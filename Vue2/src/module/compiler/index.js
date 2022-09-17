622cdc9d38889cb241f26735f4e553f296af4eeb
CrazyCatZhang <CrazyCatZhang@outlook.com>
2022-09-17 22:06:59 +0800

    Generate render function based on AST

--- Vue2/src/module/compiler/index.js
+++ Vue2/src/module/compiler/index.js
@@ -1,6 +1,13 @@
 import parseHTML from "./parse";
+import codegen from "./codegen";
+
 
 export default function compileToFunction(template) {
     const ast = parseHTML(template)
-    console.log(ast)
+    let code = codegen(ast)
+    code = `with(this){
+        return ${code}
+    }`
+    let render = new Function(code)
+    return render
 }
